feb74b10101b2d175527e0bdc8556fa3d9146265
alpapie <alpapie0908@gmail.com>
2024-05-16 13:37:45 +0000

    fix: add mobile menu

--- frontend/src/routes/header.svelte
+++ frontend/src/routes/header.svelte
@@ -1,5 +1,23 @@
+<script>
+let headermen=""
+let list=""
+let spancl=""
 
-    <header class="cs_site_header cs_style1 cs_sticky_header cs_heading_color">
+let mobile =()=>{
+  if (!headermen){
+    headermen="cs_heading_color cs_mobile_toggle_active"
+    list="cs_active"
+    spancl="cs_toggle_active"
+    return
+  }
+  headermen=""
+    list=""
+    spancl=""
+}
+
+</script>
+
+    <header class="cs_site_header cs_style1 cs_sticky_header cs_heading_color {headermen}">
         <div class="cs_main_header">
           <div class="container">
             <div class="cs_main_header_in">
@@ -8,120 +26,17 @@
                   <img src="/img/logo.svg" alt="Logo">
                 </a>
                 <nav class="cs_nav">
-                  <ul class="cs_nav_list">
+                  <ul class="cs_nav_list {list}">
                     <li ><a href="/">Acceuil</a></li>
                     <li><a href="/doctors">Medecins</a></li>
-                    <!-- <li><a href="/about">About</a></li> -->
                   </ul>
+                  <span on:click={mobile} class="cs_menu_toggle {spancl}"><span></span></span>
                 </nav>
               </div>
-              <div class="cs_main_header_right">
-                <div class="cs_toolbox">
-                  <button class="cs_toolbox_btn cs_search_toggle_btn" type="button">
-                    <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http:www.w3.org/2000/svg">
-                      <path d="M13.1684 0C5.91146 0 0 5.90944 0 13.164C0 20.4184 5.91146 26.3394 13.1684 26.3394C16.2681 26.3394 19.1188 25.2535 21.3719 23.4505L26.8571 28.931C27.1339 29.1962 27.5036 29.3426 27.887 29.3386C28.2704 29.3347 28.6371 29.1809 28.9084 28.91C29.1797 28.6392 29.3342 28.2729 29.3386 27.8896C29.3431 27.5064 29.1972 27.1365 28.9322 26.8595L23.4471 21.3762C25.2521 19.1204 26.3397 16.2662 26.3397 13.164C26.3397 5.90944 20.4254 0 13.1684 0ZM13.1684 2.926C18.8435 2.926 23.4099 7.49078 23.4099 13.164C23.4099 18.8371 18.8435 23.4134 13.1684 23.4134C7.4933 23.4134 2.92695 18.8371 2.92695 13.164C2.92695 7.49078 7.4933 2.926 13.1684 2.926Z" fill="currentColor"/>
-                    </svg>                    
-                  </button>
-                  <button class="cs_toolbox_btn cs_sidebar_toggle_btn" type="button">
-                    <svg width="35" height="30" viewBox="0 0 35 30" fill="none" xmlns="http:www.w3.org/2000/svg">
-                      <path d="M0.483887 2.46544C0.483887 1.10383 1.14618 0 1.96315 0H33.5208C34.3377 0 35 1.10383 35 2.46544C35 3.82708 34.3377 4.93088 33.5208 4.93088H1.96315C1.14618 4.93088 0.483887 3.82708 0.483887 2.46544Z" fill="currentColor"/>
-                      <path d="M0.483887 14.6694C0.483887 13.3074 1.14618 12.2039 1.96315 12.2039H33.5208C34.3377 12.2039 35 13.3074 35 14.6694C35 16.0309 34.3377 17.1348 33.5208 17.1348H1.96315C1.14618 17.1348 0.483887 16.0309 0.483887 14.6694Z" fill="currentColor"/>
-                      <path d="M0.483887 26.6267C0.483887 25.2648 1.14618 24.1613 1.96315 24.1613H33.5208C34.3377 24.1613 35 25.2648 35 26.6267C35 27.9883 34.3377 29.0922 33.5208 29.0922H1.96315C1.14618 29.0922 0.483887 27.9883 0.483887 26.6267Z" fill="currentColor"/>
-                    </svg>                    
-                  </button>
-                </div>
-              </div>
+              
             </div>
           </div>
         </div>
       </header>
-      <!-- <div class="cs_sidenav">
-        <div class="cs_sidenav_overlay"></div>
-        <div class="cs_sidenav_in">
-          <button class="cs_close" type="button"><img src="/img/icons/close.svg" alt="Close"></button>
-          <div class="cs_logo_box">
-            <img src="/img/logo.svg" alt="Logo">
-            <div class="cs_height_15"></div>
-            <h3 class="cs_fs_24 cs_semibold mb-0">Your Partner in Health and Wellness</h3>
-          </div>
-          <div class="cs_height_35"></div>
-          <hr>
-          <div class="cs_height_70 cs_height_lg_50"></div>
-          <div class="cs_iconbox cs_style_11 cs_radius_25">
-            <div class="cs_iconbox_icon">
-              <img src="/img/contact/icon_1.svg" alt="Icon">
-            </div>
-            <div class="cs_iconbox_right">
-              <h3 class="cs_iconbox_title cs_fs_24 mb-0">Phone</h3>
-              <p class="cs_iconbox_subtitle mb-0 cs_heading_color">123-456-7890</p>
-            </div>
-          </div>
-          <div class="cs_height_30"></div>
-          <div class="cs_iconbox cs_style_11 cs_radius_25">
-            <div class="cs_iconbox_icon">
-              <img src="/img/contact/icon_2.svg" alt="Icon">
-            </div>
-            <div class="cs_iconbox_right">
-              <h3 class="cs_iconbox_title cs_fs_24 mb-0">Email</h3>
-              <p class="cs_iconbox_subtitle mb-0 cs_heading_color">hellocallcenter@gmail.com</p>
-            </div>
-          </div>
-          <div class="cs_height_30"></div>
-          <div class="cs_iconbox cs_style_11 cs_radius_25">
-            <div class="cs_iconbox_icon">
-              <img src="/img/contact/icon_3.svg" alt="Icon">
-            </div>
-            <div class="cs_iconbox_right">
-              <h3 class="cs_iconbox_title cs_fs_24 mb-0">Location</h3>
-              <p class="cs_iconbox_subtitle mb-0 cs_heading_color">123 Anywhere St., Any City, 12345</p>
-            </div>
-          </div>
-          <div class="cs_height_60"></div>
-          <div class="cs_newsletter cs_style1">
-            <h2 class="cs_newsletter_title">Be Our Subscribers</h2>
-            <div class="cs_newsletter_subTitle">To get the latest news about health from our experts</div>
-            <form action="#" class="cs_newsletter_form">
-              <input type="email" class="cs_newsletter_input" placeholder="example@email.com">
-              <button class="cs_btn cs_style_1">
-                <span>Submit</span>
-                <i>
-                  <img src="/img/icons/arrow_white.svg" alt="Icon">
-                  <img src="/img/icons/arrow_white.svg" alt="Icon">
-                </i>
-              </button>
-            </form>
-          </div>
-          <div class="cs_height_70 cs_height_lg_50"></div>
-          <hr>
-          <div class="cs_height_70 cs_height_lg_50"></div>
-          <div class="cs_social_links_wrap">
-            <h2>Follow Us</h2>
-            <div class="cs_social_links">
-              <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
-              <a href="#"><i class="fa-brands fa-youtube"></i></a>
-              <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
-              <a href="#"><i class="fa-brands fa-twitter"></i></a>
-              <a href="#"><i class="fa-brands fa-instagram"></i></a>
-            </div>
-          </div>
-        </div>
-      </div> -->
-      <div class="cs_header_search">
-        <div class="cs_header_search_in">
-          <div class="container">
-            <div class="cs_header_search_box">
-              <form action="#" class="cs_search_form">
-                <input type="text" placeholder="Search Doctors">
-                <button class="cs_search_btn">
-                  <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http:www.w3.org/2000/svg">
-                    <path d="M8.07914 0C3.62682 0 0 3.62558 0 8.07641C0 12.5272 3.62682 16.1599 8.07914 16.1599C9.98086 16.1599 11.7299 15.4936 13.1122 14.3875L16.4775 17.7498C16.6473 17.9126 16.8741 18.0024 17.1094 18C17.3446 17.9975 17.5695 17.9032 17.736 17.737C17.9025 17.5708 17.9972 17.3461 17.9999 17.111C18.0027 16.8758 17.9132 16.6489 17.7506 16.4789L14.3853 13.1148C15.4928 11.7308 16.16 9.97968 16.16 8.07641C16.16 3.62558 12.5315 0 8.07914 0ZM8.07914 1.79517C11.561 1.79517 14.3625 4.59577 14.3625 8.07641C14.3625 11.557 11.561 14.3647 8.07914 14.3647C4.59732 14.3647 1.79575 11.557 1.79575 8.07641C1.79575 4.59577 4.59732 1.79517 8.07914 1.79517Z" fill="currentColor"></path>
-                  </svg>                
-                </button>
-              </form>
-              <button class="cs_close" type="button"><img src="/img/icons/close.svg" alt="Close"></button>
-            </div>
-          </div>
-        </div>
-        <div class="cs_sidenav_overlay"></div>
-      </div>
+
       <!-- End Header Section -->
